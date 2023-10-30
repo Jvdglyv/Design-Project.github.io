26eb41dcefeba6c4bb5c4ad1248a2522d6c0554c
Serkan <serkankaragoz2002@gmail.com>
2023-10-30 13:12:31 +0300

    Added dynamic mapping to website content

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,31 +5,9 @@ import React, {ReactNode, useState} from "react";
 import {Data1_1} from "../explanations/1_1";
 import "./Main.css"
 import {Menu, MenuItem, SubMenu} from "react-pro-sidebar";
-import {Data1_2} from "../explanations/1_2";
-import {Data2} from "../explanations/2";
-import {Data2_1} from "../explanations/2_1";
-import {Data2_3} from "../explanations/2_3";
-import {Data2_4} from "../explanations/2_4";
-import {Data2_5} from "../explanations/2_5";
-import {Data2_2} from "../explanations/2_2";
-import {Data2_6} from "../explanations/2_6";
-import {Data3_1} from "../explanations/3_1";
-import {Data3_2} from "../explanations/3_2.";
-import {Data4} from "../explanations/4";
-import {Data5} from "../explanations/5";
-import {Data1_4} from "../explanations/1_4";
-import {Data1_3} from "../explanations/1_3";
-import {Data3_3} from "../explanations/3_3";
-import {Data1_5} from "../explanations/1_5";
-import {Data1_6} from "../explanations/1_6";
-import {Data1_7} from "../explanations/1_7";
-import {Data1_8} from "../explanations/1_8";
-import {Data1_15} from "../explanations/1_15";
-import {Data1_9} from "../explanations/1_9";
-import {Data1_10} from "../explanations/1_10";
-import {Data6} from "../explanations/6.";
-import {Data7} from "../explanations/7";
+import {staticData} from "../extensions/StaticData";
 
+//  <MergedContent text={selectedText} onTextChange={setSelectedText}/>
 export const Main = () => {
     const [selectedText, setSelectedText] = useState<ReactNode>(Data1_1);
     return (
@@ -38,39 +16,22 @@ export const Main = () => {
             <div className={"main"}>
                 <LeftSide>
                     <Menu>
-                        <SubMenu label="1. What is Blockchain?">
-                            <MenuItem onClick = {() => setSelectedText(Data1_1)}> 1.1 - What is blockchain?</MenuItem>
-                            <MenuItem onClick = {() => setSelectedText(Data1_2)}> 1.2 - Where to use blockchain technology</MenuItem>
-                            <MenuItem onClick = {() => setSelectedText(Data1_3)}> 1.3 - How does blockchain work?</MenuItem>
-                            <MenuItem onClick = {() => setSelectedText(Data1_4)}> 1.4 - Features of blockchain</MenuItem>
-                            <MenuItem onClick = {() => setSelectedText(Data1_5)}> 1.5 - Transaction Process</MenuItem>
-                            <MenuItem onClick = {() => setSelectedText(Data1_6)}> 1.6 - Blockchain Decentralization</MenuItem>
-                            <MenuItem onClick = {() => setSelectedText(Data1_7)}> 1.7 - The Raiden Network</MenuItem>
-                            <MenuItem onClick = {() => setSelectedText(Data1_8)}> 1.8 - Bitcoin Lightning Network</MenuItem>
-                            <MenuItem onClick = {() => setSelectedText(Data1_9)}> 1.9 - SEGWIT</MenuItem>
-                            <MenuItem onClick = {() => setSelectedText(Data1_10)}> 1.10 - Bitcoin Bug</MenuItem>
-                            <MenuItem onClick = {() => setSelectedText(Data1_15)}> 1.15 - Other Blockchain Links</MenuItem>
-                        </SubMenu>
-                        <SubMenu label="2. Proofs">
-                            <MenuItem onClick = {() => setSelectedText(Data2_1)}>2.1 Proof of Work</MenuItem>
-                            <MenuItem onClick = {() => setSelectedText(Data2_2)}>2.2 Proof of Stake</MenuItem>
-                            <MenuItem onClick = {() => setSelectedText(Data2_3)}>2.3 Proof of Activity</MenuItem>
-                            <MenuItem onClick = {() => setSelectedText(Data2_4)}>2.4 Proof of Burn</MenuItem>
-                            <MenuItem onClick = {() => setSelectedText(Data2_5)}>2.5 Proof of Capacity</MenuItem>
-                            <MenuItem onClick = {() => setSelectedText(Data2_6)}>2.6 Proof of Elapsed Time</MenuItem>
-                        </SubMenu>
-                        <SubMenu label="3. Some theorems">
-                            <MenuItem onClick = {() => setSelectedText(Data3_1)}>3.1 The Cap Theorem</MenuItem>
-                            <MenuItem onClick = {() => setSelectedText(Data3_2)}>3.2 Byzantine Generals Problem</MenuItem>
-                            <MenuItem onClick = {() => setSelectedText(Data3_3)}>3.3 Zero-Knowledge Proof</MenuItem>
-                        </SubMenu>
-                        <MenuItem onClick = {() => setSelectedText(Data4)}>4. Smart Contracts</MenuItem>
-                        <MenuItem onClick={() => setSelectedText(Data5)}>5. Private vs. Public Blockchains</MenuItem>
-                        <MenuItem onClick={() => setSelectedText(Data6)}>6. Cryptocurrency Exchanges</MenuItem>
-                        <MenuItem onClick={() => setSelectedText(Data7)}>7. Stable Coin</MenuItem>
-                        
-
+                        {
+                            staticData.map((item, i) => (
+                                <SubMenu label={i + ". " + item.title}>
+                                    {
+                                        item.content.map((subItem, j) => (
+                                                <MenuItem onClick={() => setSelectedText(subItem.content)}>
+                                                    <div>{i + "." + j + " - " + subItem.title}</div>
+                                                </MenuItem>
+                                            )
+                                        )
+                                    }
+                                </SubMenu>
+                            ))
+                        }
                     </Menu>
+
                 </LeftSide>
                 <RightSide>
                     {selectedText}
---
+++ src/extensions/StaticData.tsx
@@ -0,0 +1,79 @@
+﻿import {Data1_1} from "../explanations/1_1";
+import {Data1_2} from "../explanations/1_2";
+import {Data1_3} from "../explanations/1_3";
+import {Data1_4} from "../explanations/1_4";
+import {Data1_5} from "../explanations/1_5";
+import {Data1_6} from "../explanations/1_6";
+import {Data1_7} from "../explanations/1_7";
+import {Data1_8} from "../explanations/1_8";
+import {Data1_9} from "../explanations/1_9";
+import {Data1_10} from "../explanations/1_10";
+import {Data1_15} from "../explanations/1_15";
+import {Data2_1} from "../explanations/2_1";
+import {Data2_2} from "../explanations/2_2";
+import {Data2_3} from "../explanations/2_3";
+import {Data2_4} from "../explanations/2_4";
+import {Data2_5} from "../explanations/2_5";
+import {Data3_1} from "../explanations/3_1";
+import {Data3_2} from "../explanations/3_2";
+import {Data3_3} from "../explanations/3_3";
+import {Data4} from "../explanations/4";
+import {Data5} from "../explanations/5";
+import {Data6} from "../explanations/6";
+import {Data7} from "../explanations/7";
+import {Data2_6} from "../explanations/2_6";
+
+export const staticData= 
+    [
+        {
+            "title": "What is Blockchain?",
+            "content":
+                [
+                    {"title": "What is blockchain?", "content": Data1_1},  {"title": "", "content": Data1_2}, {"title": "", "content": Data1_3}, {"title": "", "content": Data1_4}, {"title": "", "content": Data1_5}, {"title": "", "content": Data1_6}, {"title": "", "content": Data1_7}, {"title": "", "content": Data1_8}, {"title": "", "content": Data1_9}, {"title": "", "content": Data1_10}, {"title": "", "content": Data1_15}
+                ]
+        },
+        {
+            "title": "Proofs",
+            "content":
+                [
+                    {"title": "Proof of Work", "content": Data2_1}, {"title": "Proof of Stake", "content": Data2_2}, {"title": "Proof of Activity", "content": Data2_3}, {"title": "Proof of Burn", "content": Data2_4}, {"title": "Proof of Capacity", "content": Data2_5}, {"title": "Proof of Elapsed Time", "content": Data2_6}   
+                ]
+        },
+        {
+            "title": "Some theorems",
+            "content":
+                [
+                    {"title": "The Cap Theorem", "content": Data3_1}, {"title": "Byzantine Generals Problem", "content": Data3_2}, {"title": "Zero-Knowledge Proof", "content": Data3_3}
+                ]
+        },
+        {
+            "title": "Smart Contracts",
+            "content":
+                [
+                    {"title": "Smart Contracts", "content": Data4}
+                ]
+        },
+        {
+            "title": "Private vs. Public Blockchains",
+            "content":
+                [
+                    {"title": "Private vs. Public Blockchains", "content": Data5}
+                ]
+        },
+        {
+            "title": "Cryptocurrency Exchanges",
+            "content":
+                [
+                    {"title": "Cryptocurrency Exchanges", "content": Data6}
+                ]
+        },
+        {
+            "title": "Stable Coin",
+            "content":
+                [
+                    {"title": "Stable Coin", "content": Data7}
+                ]
+        },
+        
+        
+    ] 
