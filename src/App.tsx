886baa8207bf315ff92c3dae296eb1c0764ccb0e
YasinSimsek2000 <simsekyasin2900@gmail.com>
2023-12-02 18:48:47 +0300

    readme deleted

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
     <div className="App">
         <BrowserRouter>
             <Routes>
+                <Route path="" element={<Main />}></Route>
                 <Route path="/home" element={<Main />}></Route>
                 <Route path="/solana" element={<Solana />}></Route>
             </Routes>
